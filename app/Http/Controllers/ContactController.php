15ecf183001a4cb75471abbae969888d4c8e366a
Indra <indra@ide.web.id>
2021-05-25 17:09:21 +0700

    add validation rule ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,11 +13,11 @@ class ContactController extends Controller
 	public function store(Request $request)
 	{
 		$request->validate([
-			'first_name' => 'required|string',
-			'last_name' => 'required|string',
-			'email' => 'required|email',
-			'phone' => 'required|string',
-			'message' => 'required'
+			'first_name' => 'required|string|min:2|max:20',
+			'last_name' => 'required|string|min:2|max:20',
+			'email' => 'required|email|min:5|max:37',
+			'phone' => 'required|string|min:5|max:15',
+			'message' => 'required|min:10|max:200'
 		]);
 		
 		$contact = new Contact();
